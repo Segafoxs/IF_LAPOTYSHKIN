c9242ce5e9eab72c7f6f9e10a96b8cd8b6335a8b
Sergey Lapotyshkin <segafox2@mail.ru>
2025-04-21 16:31:39 +0500

    Добавлены классы Car, CarFabric, CheckCar

--- src/main/java/ru/iFellow/MainClass.java
+++ src/main/java/ru/iFellow/MainClass.java
@@ -1,5 +1,14 @@
 package ru.iFellow;
+import java.util.*;
+
 
 public class MainClass {
-    //it's Main origin
+    public static void main(String[] args) {
+
+        List<Car> cars = new ArrayList<>();
+        CarFabric.addCarList(cars);
+        CheckCar.checkYearCar(cars);
+        CheckCar.checkColorCar(cars);
+        CheckCar.checkEnginePower(cars);
+    }
 }
